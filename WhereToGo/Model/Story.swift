0b76b0b8668d666232b46632ea54663d48bb4203
GrigoryDushin <g.s.dushin@gmail.com>
2025-02-08 18:34:54 +0100

    done

---
+++ WhereToGo/Model/Story.swift
@@ -0,0 +1,16 @@
+//
+//  Story.swift
+//  WhereToGo
+//
+//  Created by Григорий Душин on 08.02.2025.
+//
+
+import Foundation
+
+struct Story {
+    let title: String
+    let choice1: String
+    let choice1Destination: Int
+    let choice2: String
+    let choice2Destination: Int
+}
